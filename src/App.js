f8751d78cbfa010b154fb04fa4534160f835b835
alialmahdawi <33576289+alialmahdawi@users.noreply.github.com>
2018-05-19 12:28:07 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import './App.css';
 
 function Aboutme(){
   return(
-    <body className='mine'>
+    <div className='mine'>
     <h3 className='myname'>My Name Is Ali</h3>
     <p className='bio'>I'm from Iraq , I like nothing and i'm pretty boring :)</p>
     
     <img src='https://i.ytimg.com/vi/DNcwW_mQ_pU/hqdefault.jpg' className='picture' alt='baby'/>
     <label className='label'>This is how i used to look when i was kid</label>
     
-    </body>
+    </div>
   );
 }
 
